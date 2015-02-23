60fb2149db71add55b859de05d7cd221e61737f2
Marcel Voigt <marcelpvoigt@gmail.com>
2015-02-23 19:53:47 +0100

    Format/indent source

--- src/Mihaeu/HtmlFormatter.php
+++ src/Mihaeu/HtmlFormatter.php
@@ -15,6 +15,7 @@ class HtmlFormatter
 	 * @param string $html HTML string.
 	 * @param string $indentWith Characters that are being used for indentation (default = 4 spaces).
 	 * @param string $tagsWithoutIndentation Comma-separated list of HTML tags that should not be indented (default = html,link,img,meta)
+	 *
 	 * @return string Re-indented HTML.
 	 */
 	public static function format($html, $indentWith = '    ', $tagsWithoutIndentation = 'html,link,img,meta')
@@ -25,40 +26,28 @@ class HtmlFormatter
 		$dom = self::parseDom($elements);
 
 		$indent = 0;
-		$output = array();
-		foreach ($dom as $index => $element)
-		{
-			if ($element['opening'])
-			{
-				$output[] = "\n".str_repeat($indentWith, $indent).trim($element['content']);
+		$output = [];
+		foreach ($dom as $index => $element) {
+			if ($element['opening']) {
+				$output[] = "\n" . str_repeat($indentWith, $indent) . trim($element['content']);
 
 				// make sure that only the elements who have not been blacklisted are being indented
-				if ( ! in_array($element['type'], explode(',', $tagsWithoutIndentation)))
-				{
+				if (!in_array($element['type'], explode(',', $tagsWithoutIndentation))) {
 					++$indent;
 				}
-			}
-			else if ($element['standalone'])
-			{
-				$output[] = "\n".str_repeat($indentWith, $indent).trim($element['content']);
-			}
-			else if ($element['closing'])
-			{
+			} else if ($element['standalone']) {
+				$output[] = "\n" . str_repeat($indentWith, $indent) . trim($element['content']);
+			} else if ($element['closing']) {
 				--$indent;
-				$lf = "\n".str_repeat($indentWith, $indent);
-				if (isset($dom[$index - 1]) && $dom[$index - 1]['opening'])
-				{
+				$lf = "\n" . str_repeat($indentWith, $indent);
+				if (isset($dom[$index - 1]) && $dom[$index - 1]['opening']) {
 					$lf = '';
 				}
-				$output[] = $lf.trim($element['content']);
-			}
-			else if ($element['text'])
-			{
-				$output[] = "\n".str_repeat($indentWith, $indent).preg_replace('/ [ \t]*/', ' ', $element['content']);
-			}
-			else if ($element['comment'])
-			{
-				$output[] = "\n".str_repeat($indentWith, $indent).trim($element['content']);
+				$output[] = $lf . trim($element['content']);
+			} else if ($element['text']) {
+				$output[] = "\n" . str_repeat($indentWith, $indent) . preg_replace('/ [ \t]*/', ' ', $element['content']);
+			} else if ($element['comment']) {
+				$output[] = "\n" . str_repeat($indentWith, $indent) . trim($element['content']);
 			}
 		}
 
@@ -66,17 +55,17 @@ class HtmlFormatter
 	}
 
 	/**
-	 * Parses an array of HTML tokens and adds basic information about about the type of 
+	 * Parses an array of HTML tokens and adds basic information about about the type of
 	 * tag the token represents.
 	 *
 	 * @param Array $elements Array of HTML tokens (tags and text tokens).
+	 *
 	 * @return Array HTML elements with extra information.
 	 */
 	public static function parseDom(Array $elements)
 	{
-		$dom = array();
-		foreach ($elements as $element)
-		{
+		$dom = [];
+		foreach ($elements as $element) {
 			$isText = false;
 			$isComment = false;
 			$isClosing = false;
@@ -85,36 +74,27 @@ class HtmlFormatter
 
 			$currentElement = trim($element);
 
-			if (strpos($currentElement, '<!') === 0)
-			{
+			if (strpos($currentElement, '<!') === 0) {
 				$isComment = true;
-			}
-			else if (strpos($currentElement, '</') === 0)
-			{
+			} else if (strpos($currentElement, '</') === 0) {
 				$isClosing = true;
-			}
-			else if (preg_match('/\/>$/', $currentElement))
-			{
+			} else if (preg_match('/\/>$/', $currentElement)) {
 				$isStandalone = true;
-			}
-			else if (strpos($currentElement, '<') === 0)
-			{
+			} else if (strpos($currentElement, '<') === 0) {
 				$isOpening = true;
-			}
-			else
-			{
+			} else {
 				$isText = true;
 			}
 
-			$dom[] = array(
-				'text' 				=> $isText,
-				'comment'			=> $isComment,
-				'closing'	 		=> $isClosing,
-				'opening'	 		=> $isOpening,
-				'standalone'	 	=> $isStandalone,
-				'content'			=> $element,
-				'type'				=> preg_replace('/^<\/?(\w+)[ >].*$/U', '$1', $element)
-			);
+			$dom[] = [
+				'text'       => $isText,
+				'comment'    => $isComment,
+				'closing'    => $isClosing,
+				'opening'    => $isOpening,
+				'standalone' => $isStandalone,
+				'content'    => $element,
+				'type'       => preg_replace('/^<\/?(\w+)[ >].*$/U', '$1', $element)
+			];
 		}
 		return $dom;
 	}
